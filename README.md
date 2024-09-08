6e95497ef593f661e354e0935ea8998a1863800e
saurabh goyal <89391860+The100god@users.noreply.github.com>
2024-09-08 11:57:55 +0530

    Assignment 2: JavaScript Basics complete.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # interactive-greeting
+
+interactive-greeting/
+│
+├── interactive.html 
+├── styles.css
+├── script.js
+└── README.md 
+
+# Instruction
+- First clone the repo https://github.com/The100god/interactive-greeting.git in your code editor
+- Run the file and see interactive-greeting page.
+
+### Features
+
+- Made by using HTML, CSS, JAVASCRIPT.
+- Give Your name as input in input field.
+- Get a nice greeting after click greet button.
+- Error handling.
+- Animated greeting text.
--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 document.getElementById('greetBtn').addEventListener('click', function () {
     var nameInput = document.getElementById('nameInput').value;
-    var greetingMessage = document.getElementById('greetingMessage');
+    var greetMessage = document.getElementById('greetMessage');
+    var greetingMessage1 = document.getElementById('greetingMessage1');
+    var greetingMessage2 = document.getElementById('greetingMessage2');
     var errorMessage = document.getElementById('errorMessage');
+    greetMessage.style.transform="scaleY(0)";
+    greetMessage.style.transition="transform 0.1s ease-in-out";
     
+    // greetMessage.style.display="";
     if (nameInput === "") {
         errorMessage.textContent = "Please enter your name.";
-        greetingMessage.classList.remove('show');
+        greetMessage.style.transform="scaleY(1)";
+        greetMessage.style.transition="transform 1s ease-in-out";
+        greetingMessage1.classList.remove('show');
+        greetingMessage2.classList.remove('show');
+        setTimeout(()=>{
+            // greetMessage.style.transform="scaleX(0)";
+            greetMessage.style.transform="scaleY(0)";
+            greetMessage.style.transition="transform 0.5s ease-in-out";
+        }, 3000)
     } else {
         errorMessage.textContent = "";
-        greetingMessage.textContent = "Hello, " + nameInput + "!";
-        greetingMessage.classList.add('show');
+        greetMessage.style.transform="scaleY(1)";
+        greetMessage.style.transition="transform 1s ease-in-out";
+        greetingMessage1.textContent = "Hello, " + nameInput + "!";
+        greetingMessage2.textContent = "Nice to meet you.";
+        greetingMessage1.classList.add('show');
+        greetingMessage2.classList.add('show');
+        document.getElementById('nameInput').value = "";
     }
 });
+
